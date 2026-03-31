3683cf3f941a21f7a8160a807a9b867f3e0dc1e1
EstrHuP <estrhup@gmail.com>
2026-04-01 01:10:39 +0200

    feat(frontend): -Cypress: testing add and edit match

--- frontend/cypress/e2e/admin.cy.ts
+++ frontend/cypress/e2e/admin.cy.ts
@@ -1,13 +1,50 @@
-describe('Login con admin', () => {
+describe('Ejecuciones como admin', () => {
+  // Login
   it('Admin inicia sesión', () => {
     cy.visit('/login');
-    cy.get('input[name=email').type('admin');
+    cy.get('input[name=email]').type('admin');
     cy.get('input[name=password]').type('a');
     cy.get('button[type=submit').click();
+    cy.url().should('include', '/index_admin');
   });
-  
+
+  // Nuevo partido
+  it('Admin crea un nuevo partido', () => {
+    cy.visit('/index_admin');
+    cy.get('input[name=equipo1').type('testEquipo1Cypress');
+    cy.get('input[name=equipo2').type('testEquipo2Cypress');
+    cy.get('input[name=arbitro').type('testArbitroCypress');
+    cy.get('input[name=ubicacion').type('testUbiCypress');
+    cy.get('button[type=submit').click();
+    cy.get('td').should('contain', 'testEquipo1Cypress');
+
+    // Validación visual: 
+    // - se ve la tabla 
+    // - aparece la fila con el dato nuevo
+    cy.contains('h3', 'Partidos existentes').should('be.visible');
+    cy.get('table.table').should('be.visible');
+    cy.contains('tbody tr', 'testEquipo1Cypress').should('be.visible');
+  });
+
+  // Editar partido
+  it('Admin edita un partido', () => {
+    cy.visit('/index_admin');
+    cy.get('tbody tr').eq(2).within(() => { // Edita el equipo local de la tercera fila
+      cy.contains('button', 'Editar').click(); // busca boton editar
+      cy.get('input').first().clear().type('Equipo Local Editado'); //edita el field de equiop local
+      cy.contains('button', 'Guardar').click(); // guarda
+      cy.contains('button', 'Cancelar').click(); // cancela el evento
+    });
+    // Validación visual: 
+    // - se ve la fila editada
+    cy.contains('h3', 'Partidos existentes').should('be.visible');
+    cy.get('table.table').should('be.visible');
+    cy.contains('tbody tr', 'Equipo Local Editado').should('be.visible');
+  });
+
+  // Accede al listado de jugadores
   it('Admin accede a lista jugadores', () => {
-    cy.visit('/players'); // ruta de tu app
+    cy.visit('/players');
 
     cy.contains('Búsqueda de jugadores').should('be.visible');
 
